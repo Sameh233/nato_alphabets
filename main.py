64acce57b02cdc0badbd50ec3fddcc32b5ff37e9
samehBgit <samehbelaid04@gmail.com>
2024-10-03 22:25:56 +0100

    main_updated

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ df = pd.read_csv("nato_phonetic_alphabet.csv")
 
 nato_alphabet_dict = {row.letter : row.code for (index,row) in df.iterrows()}
 
-user_input = input("Enter a word: ").upper()
+def nato_alphabet():
+    user_input = input("Enter a word: ").upper()
+    try:
+        result = [nato_alphabet_dict[letter] for letter in user_input]     
+    except KeyError :
+        print("Sorry, only letters in the alphabet please.")
+        nato_alphabet()
+    else:
+        print(result)
 
-# getting the 
-result = [nato_alphabet_dict[letter] for letter in user_input]
-           
-print(result)
+nato_alphabet()
+        
 
